4999630709e8c13c806fdd90f31520813918cf6d
andreasadinata <andreasadinata@hotmail.com>
2017-07-05 18:17:20 -0700

    added component test for react

---
+++ public/components/logo.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    shallow
+}
+from 'enzyme';
+
+import Logo from './logo';
+
+describe('<Logo />', () => {
+    it('Renders without crashing', () => {
+        shallow( < Logo / > );
+                });
+    });
---
+++ public/components/whyTravel.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    shallow
+}
+from 'enzyme';
+
+import WhyTravel from './whyTravel';
+
+describe('<WhyTravel />', () => {
+    it('Renders without crashing', () => {
+        shallow( < WhyTravel / > );
+                });
+    });
